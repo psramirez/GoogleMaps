04a66a22f2656d7bad243c61ebae282860ac8255
psramirez <pablillo.es@gmail.com>
2014-01-12 14:41:26 +0100

    recuperar coordenadas

--- js/init.js
+++ js/init.js
@@ -1,5 +1,17 @@
 $(document).on('ready',function(){
 	
-	alert("jquery");
+	//coordenada inicial
+	var config = {
+	zoom:13,
+	center: new google.maps.LatLng(40.473330251762974,-3.8682174682617188),
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	//crear mapa
+	var mapa = new google.maps.Map($('#mapa')[0],config);
+	
+	//crear un marcador
+	google.maps.event.addListener(mapa, 'click', function(event) {
+    		console.log(event.latLng);//event.latLng(retorna coordenadas)
+  	});
 
 })

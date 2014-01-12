8983e0f6eebb41b1e277341a3e38945e4fd717a0
psramirez <pablillo.es@gmail.com>
2014-01-12 17:59:24 +0100

    empezando con ajax

--- js/init.js
+++ js/init.js
@@ -1,4 +1,41 @@
+	//ARRAY PARA ALMACENAR NUEVOS MARCADORES
+    var marcadores_nuevos = [];
+
+	//FUNCION PARA QUITAR MARCADORES DE MAPA
+    function quitar_marcadores(lista)
+    {
+        //RECORRER EL ARRAY DE MARCADORES
+        for (i in lista)
+        {
+            //QUITAR MARCADOR DEL MAPA
+            lista[i].setMap(null);
+        }
+    }
+
 $(document).on('ready',function(){
+	var formulario = $("#formulario");
+	//ajax
+	 $("#btn_grabar").on("click", function(){
+            //INSTANCIAR EL FORMULARIO
+            var f = $("#formulario");
+           $.ajax({
+               type:"POST",
+               url:"./php/iajax.php",
+               dataType:"JSON",
+               data:formulario.serialize()+"&tipo=grabar",
+               success:function(data){
+                   alert(data.mensaje);
+               },
+               beforeSend:function(){
+                   
+               },
+               complete:function(){
+                   
+               }
+           });
+           return false;
+        });
+
 	
 	//coordenada inicial
 	var config = {
@@ -11,23 +48,28 @@ $(document).on('ready',function(){
 	var marcador;
 	
 	//añadir marcador
-	google.maps.event.addListener(mapa, 'click', function(event) { 	
-	 	var coordenadas = event.latLng;			
-		//variable para marcador
-        var marcador = new google.maps.Marker({
-           titulo:prompt("Titulo del marcador?"),
-           position:coordenadas,//la posición del nuevo marcador
-           map: mapa, //en que mapa se ubicará el marcador
-           animation:google.maps.Animation.DROP,//como aparecerá el marcador
-           draggable:true//no permitir el arrastre del marcador
-       });
+		google.maps.event.addListener(mapa, 'click', function(event) { 	
+		 	var coordenadas = event.latLng;	
+		 	console.log(coordenadas.b);
+			//variable para marcador
+	        var marcador = new google.maps.Marker({          
+	           position:coordenadas,//la posición del nuevo marcador
+	           map: mapa, //en que mapa se ubicará el marcador
+	           animation:google.maps.Animation.DROP,//como aparecerá el marcador
+	           draggable:true//no permitir el arrastre del marcador
+	       });
+	        marcadores_nuevos.push(marcador);
+	    	//agregar evento click al marcador
+	       google.maps.event.addListener(marcador, "click", function(){
+	          
+	   	   });
 
-    	//agregar evento click al marcador
-       google.maps.event.addListener(marcador, "click", function(){
-          //mostrar una alerta al hacer  click
-          alert(marcador.titulo);
-       });
-       
+	   
+       formulario.find("input[name='cx']").val(coordenadas.b);
+       formulario.find("input[name='cy']").val(coordenadas.d);
+       formulario.find("input[name='titulo']").focus(); 
+
+        quitar_marcadores(marcadores_nuevos);
        //ubicar el marcador en el mapa
        marcador.setMap(mapa);  	 
   	});
